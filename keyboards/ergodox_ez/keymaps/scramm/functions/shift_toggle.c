2cd83c93c9b9494b9d6c739f04f4194b653bf29f
Steven Cramm <steven.cramm@gmail.com>
2019-06-17 09:55:04 -0500

    simplify key pressed event

--- keyboards/ergodox_ez/keymaps/scramm/functions/shift_toggle.c
+++ keyboards/ergodox_ez/keymaps/scramm/functions/shift_toggle.c
@@ -1,4 +1,3 @@
-
 // checks whether left shift is being held
 bool is_lsft_pressed(void) {
 	if ((get_mods() & (MOD_BIT(KC_LSFT))) != 0) {
@@ -10,11 +9,7 @@ bool is_lsft_pressed(void) {
 // acts like LT normally, or TG when used with left shift
 bool custom_layer_toggle(int layer, keyrecord_t *record) {
 	if (record->event.pressed) {
-		if (is_lsft_pressed()) {
-			layer_invert(layer);
-		} else {
-			layer_on(layer);
-		}
+		layer_on(layer);
 	} else {
 		if (!is_lsft_pressed()) {
 			layer_off(layer);
